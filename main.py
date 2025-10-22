fc4fa5f2d2db267e75ecae36432ce300c9cf56ae
Adea <adea.hafnor@gmail.com>
2025-10-22 16:06:28 +0000

    la til def for opperativsystemets navn, printer ut for å teste om det fungerer

--- main.py
+++ main.py
@@ -1,1 +1,8 @@
 import os as opperativsystem
+
+def hvilketOprativsystem():
+    #Retunerer navnet på det nåværende opprativsystemmet
+    osNavn = opperativsystem.name
+    return osNavn
+
+print("Det nåværende opprativsystemmet er:", hvilketOprativsystem())
