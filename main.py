5d15f67beb0022de2c5ea4602c6afb3d4910a07d
Adea <adea.hafnor@gmail.com>
2025-10-23 13:05:27 +0000

    La til def for hvor mye ledig lagringsplass (i byte), def som sier hvem brukeren er  (fungerer kun på lokale programmer derfor er printen #-et ut. Lagt til hvilken IP-adresse brukeren har

--- main.py
+++ main.py
@@ -6,3 +6,29 @@ def hvilketOprativsystem():
     return osNavn
 
 print("Det nåværende opprativsystemmet er:", hvilketOprativsystem())
+
+def hvorMyeLedigPlass():
+    #Retunerer mengden ledig plass på disken i byte
+    statvfs = opperativsystem.statvfs('/')
+    ledigPlass = statvfs.f_bavail * statvfs.f_frsize
+    return ledigPlass
+
+print("Mengden ledig plass på disken er:", hvorMyeLedigPlass(), "byte")
+
+def hvemBrukerErLoggetInn():
+    #Retunerer navnet på den nåværende innloggede brukeren
+    brukerNavn = opperativsystem.getlogin()
+    return brukerNavn
+
+#print(f"Den nåværende innloggede brukeren er: {hvemBrukerErLoggetInn()}")
+
+def hvilkenIPAdresse():
+    #Retunerer den nåværende IP-adressen til maskinen
+    import socket
+    brukerNavn = socket.gethostname()
+    ipAdresse = socket.gethostbyname(brukerNavn)
+    return ipAdresse
+
+print("Den nåværende IP-adressen til maskinen er:", hvilkenIPAdresse())
+
+
