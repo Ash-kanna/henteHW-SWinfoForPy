f97a4530942d73978fe1558f07cc5311cc319b3e
Adea <adea.hafnor@gmail.com>
2025-10-24 08:32:31 +0000

    Lagde def som skriver ut hvilket programmer som er innstallert på maskinen. Flyttet alle printene nederst på .py-en for ryddigere oppsett.

--- main.py
+++ main.py
@@ -5,7 +5,6 @@ def hvilketOprativsystem():
     osNavn = opperativsystem.name
     return osNavn
 
-print("Det nåværende opprativsystemmet er:", hvilketOprativsystem())
 
 def hvorMyeLedigPlass():
     #Retunerer mengden ledig plass på disken i byte
@@ -13,14 +12,12 @@ def hvorMyeLedigPlass():
     ledigPlass = statvfs.f_bavail * statvfs.f_frsize
     return ledigPlass
 
-print("Mengden ledig plass på disken er:", hvorMyeLedigPlass(), "byte")
 
 def hvemBrukerErLoggetInn():
     #Retunerer navnet på den nåværende innloggede brukeren
     brukerNavn = opperativsystem.getlogin()
     return brukerNavn
 
-#print(f"Den nåværende innloggede brukeren er: {hvemBrukerErLoggetInn()}")
 
 def hvilkenIPAdresse():
     #Retunerer den nåværende IP-adressen til maskinen
@@ -29,6 +26,28 @@ def hvilkenIPAdresse():
     ipAdresse = socket.gethostbyname(brukerNavn)
     return ipAdresse
 
+
+def instalerteProgrammer():
+    #Skriver ut de installerte programmene på systemet
+    if opperativsystem.name == 'posix': #Linux eller MacOS
+        try:
+            with open('/var/lib/dpkg/status', 'r') as filLeser:
+                data = filLeser.read()
+                #liste over installerte programmer blir hentet fra dpkg status filen
+                #dpkg beholder informasjon om installerte pakker på Linux og MacOS systemer
+                programmer = [line.split(': ')[1] for line in data.split('\n') if line.startswith('Package: ')]
+                print("Instalerte programmer på systemet:")
+                for program in programmer:
+                    print(program)
+        except FileNotFoundError:
+            print("Kunne ikke finne listen over installerte programmer.")
+    else:
+        print("Vi kan ikke vsie de instalerte programmene.")
+
+
+
+print("Det nåværende opprativsystemmet er:", hvilketOprativsystem())
+print("Mengden ledig plass på disken er:", hvorMyeLedigPlass(), "byte")
+print("Den nåværende innloggede brukeren er: ", hvemBrukerErLoggetInn())
 print("Den nåværende IP-adressen til maskinen er:", hvilkenIPAdresse())
-
-
+print("Skriver ut de instalerte programmene: \n", instalerteProgrammer())
